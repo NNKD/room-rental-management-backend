d06e0ad9fd772d2c049f335e6e820f97db871a58
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2025-06-16 11:17:26 +0700

    feat(user account): add user accoutn change name, change pass

--- src/main/java/com/roomrentalmanagementbackend/controller/DashBoardUserController.java
+++ src/main/java/com/roomrentalmanagementbackend/controller/DashBoardUserController.java
@@ -1,19 +1,13 @@
 package com.roomrentalmanagementbackend.controller;
 
+import com.cloudinary.Api;
 import com.roomrentalmanagementbackend.dto.ApiResponse;
-import com.roomrentalmanagementbackend.dto.apartment.ApartmentDTO;
-import com.roomrentalmanagementbackend.dto.apartment.ApartmentStatusDTO;
-import com.roomrentalmanagementbackend.dto.apartment.ApartmentTypeDTO;
-import com.roomrentalmanagementbackend.dto.apartment.UtilityServiceDTO;
-import com.roomrentalmanagementbackend.dto.apartment.response.ApartmentManagementResponse;
 import com.roomrentalmanagementbackend.dto.apartment.response.UserApartmentDetailResponse;
+import com.roomrentalmanagementbackend.dto.user.request.UserAccountPassRequest;
+import com.roomrentalmanagementbackend.dto.user.request.UserAccountUsernameRequest;
+import com.roomrentalmanagementbackend.dto.user.response.UserAccountResponse;
 import com.roomrentalmanagementbackend.dto.user.response.UserInfoDTO;
-import com.roomrentalmanagementbackend.service.ApartmentService;
-import com.roomrentalmanagementbackend.service.ApartmentStatusService;
-import com.roomrentalmanagementbackend.service.ApartmentTypeService;
-import com.roomrentalmanagementbackend.service.UtilityServiceService;
-import com.roomrentalmanagementbackend.utils.MessageUtils;
-import jakarta.validation.Valid;
+import com.roomrentalmanagementbackend.service.*;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -22,8 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Slf4j
 @RestController
 @RequestMapping("/dashboard-user")
@@ -31,6 +23,7 @@ import java.util.List;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class DashBoardUserController {
     ApartmentService apartmentService;
+    UserService userService;
 
     @GetMapping("/me/apartments")
     public ApiResponse getApartmentUserByUsername(Authentication authentication) {
@@ -42,6 +35,30 @@ public class DashBoardUserController {
     public ApiResponse<UserApartmentDetailResponse> getApartmentDetailUser(@PathVariable String slug, Authentication authentication) {
         UserInfoDTO user = (UserInfoDTO) authentication.getPrincipal();
         return apartmentService.getApartmentDetailUser(user.getUsername(), slug);
+    }
+
+    @GetMapping("me/account")
+    public ApiResponse<UserAccountResponse> getAccount(Authentication authentication) {
+        UserInfoDTO user = (UserInfoDTO) authentication.getPrincipal();
+        return ApiResponse.success(userService.getUserAccount(user.getUsername()));
+    }
+
+    @PutMapping("me/account/update-username")
+    public ApiResponse updateName(UserAccountUsernameRequest request, Authentication authentication) {
+        if (!userService.checkValidUsername(request.getUsername())) {
+            ApiResponse.error(HttpStatus.BAD_REQUEST, "Đã tồn tại username");
+        }
+        UserInfoDTO user = (UserInfoDTO) authentication.getPrincipal();
+        return userService.updateUserAccount(user.getUsername(), request.getUsername());
+    }
+
+    @PutMapping("me/account/update-pass")
+    public ApiResponse updatePass(UserAccountPassRequest request, Authentication authentication) {
+        UserInfoDTO user = (UserInfoDTO) authentication.getPrincipal();
+        if (!userService.checkPass(user.getUsername(), request.getPass())) {
+            ApiResponse.error(HttpStatus.BAD_REQUEST, "Đã tồn tại username");
+        }
+        return userService.updateUserPass(user.getUsername(), request.getNewPass());
     }
 
 }
---
+++ src/main/java/com/roomrentalmanagementbackend/dto/user/request/UserAccountUsernameRequest.java
@@ -0,0 +1,13 @@
+package com.roomrentalmanagementbackend.dto.user.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserAccountUsernameRequest {
+    String username;
+}
